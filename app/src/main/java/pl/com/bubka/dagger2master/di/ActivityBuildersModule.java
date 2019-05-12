19540a0519462ee58de9e9ba7ec6c4bae89c23d7
Maksymilian Wojcik <maksymilian.wojcik@acasus.com>
2019-05-12 17:38:18 +0200

    Added activity injector (previous commit)

--- app/src/main/java/pl/com/bubka/dagger2master/di/ActivityBuildersModule.java
+++ app/src/main/java/pl/com/bubka/dagger2master/di/ActivityBuildersModule.java
@@ -8,6 +8,7 @@ import pl.com.bubka.dagger2master.AuthActivity;
 @Module
 public abstract class ActivityBuildersModule {
 
+    //Musimy dodac @ContributesAndroidInejctor dla kazdej activity jaka bedziemy dodawac
     @ContributesAndroidInjector //wszedzie gdzie to uzywamy to klasa musi byc abstrackyjna
     abstract AuthActivity contributeAuthActivity();
 
